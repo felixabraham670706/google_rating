a1cfccdb4370ec3bf0cb4b5e4bfab3af25d51657
felixabraham670706 <felixabraham670706@gmail.com>
2026-05-10 21:14:51 +0400

    Initial Google review automation setup

---
+++ config.py
@@ -0,0 +1,140 @@
+# Configurations for google reviews
+
+# List of all the branches that we want to study
+list_of_links = {
+'ENBD - Abu Dhabi - Abu Dhabi Main Office branch' : 'https://maps.app.goo.gl/e8LRD3JenGskh8Gd7'
+
+
+}
+
+region_to_branch = {
+    'Abu Dhabi & Al Ain Region': [
+        'ENBD - Abu Dhabi - Abu Dhabi Main Office branch',
+        'ENBD - Abu Dhabi - Electra Street Branch',
+        'ENBD - Abu Dhabi - Tourist Club Branch',
+        'ENBD - Abu Dhabi - Al Ain Khalifa street Branch',
+        'ENBD - Abu Dhabi - Dalma Mall Branch',
+        'ENBD - Abu Dhabi - Khalifa Branch',
+        'ENBD - Abu Dhabi - Khalidiya Branch',
+        'ENBD - Abu Dhabi - Reem Mall Branch',
+        'ENBD - Abu Dhabi - Yas Mall Branch',
+        'ENBD - Abu Dhabi - Deerfields Mall Branch',
+        'ENBD - Abu Dhabi - Al Ain Main Branch Hazza Bin Zayed Stadium Branch',
+        'ENBD - Abu Dhabi - Mussafah Branch',
+        'ENBD - Abu Dhabi - Madinati Mall Branch'
+    ],
+    'Bur Dubai Region': [
+        'ENBD - Dubai - Dubai Design District Branch',
+        'ENBD - Dubai - Dubai Mall Branch',
+        'ENBD - Dubai - Business Bay Branch',
+        'ENBD - Dubai - Burj Khalifa Branch',
+        'ENBD - Dubai - Al Mankhool Branch',
+        'ENBD - Dubai - Sheikh Zayed Road Branch',
+        'ENBD - Dubai - Wafi Mall Branch',
+        'ENBD - Dubai - Bank Street Branch',
+        'ENBD - Dubai - Emirates Towers Branch',
+        'ENBD - Dubai - Nad AL Shiba Branch',
+        'ENBD - Dubai - Nad Al Shiba Mall Branch',
+        'ENBD - Dubai - Oud Metha Branch',
+        'ENBD - Dubai - Al Fahidi Branch',
+        'ENBD - Dubai - Al Satwa Branch'
+    ],
+    'Deira Region': [
+        'ENBD - Dubai - Emirates Airline HQ Branch',
+        'ENBD - Dubai - Dubai Airport T3 Branch',
+        'ENBD - Dubai - Ras Al Khor Branch',
+        'ENBD - Dubai - Deira City Center Branch',
+        'ENBD - Dubai - Al Muraqqabat Branch',
+        'ENBD - Dubai - Dubai Festival City Branch',
+        'ENBD - Dubai - Dubai Silicon Oasis Branch',
+        'ENBD - Dubai - Hatta Branch',
+        'ENBD - Dubai - Al Aweer Branch',
+        'ENBD - Dubai - Group Head Office Branch',
+        'ENBD - Dubai - Al Qusais Branch',
+        'ENBD - Dubai - Deira Branch',
+        'ENBD - Dubai - Mirdif City Center Branch',
+        'ENBD - Dubai - Al Twar Branch',
+        'ENBD - Dubai - Mizher Mall Branch',
+        'ENBD - Dubai - Rashidiya Branch',
+        'ENBD - Dubai - Hamriya Branch',
+        'ENBD - Dubai - Al Qiyadah Branch'
+    ],
+    'New Dubai Region': [
+        'ENBD - Dubai - Burj Al Arab Branch',
+        'ENBD - Dubai - Jumeirah Beach Residence Branch',
+        'ENBD - Dubai - The Greens Branch',
+        'ENBD - Dubai - Dubai Media City Branch',
+        'ENBD - Dubai - Al Barsha Branch',
+        'ENBD - Dubai - Al Quoz Branch',
+        'ENBD - Dubai - Dubai Hills Mall Branch',
+        'ENBD - Dubai - Jumeirah Branch',
+        'ENBD - Dubai - Umm Suqeim Branch',
+        'ENBD - Dubai - Jumeirah Beach Road Branch',
+        'ENBD - Dubai - Emirates Mall Branch',
+        'ENBD - Dubai - Ibn Battuta Mall Branch',
+        'ENBD - Dubai - Jumeirah Lake Tower Branch',
+        'ENBD - Dubai - Jebel Ali Branch',
+        'ENBD - Dubai - Junction Mall Branch',
+        'ENBD - Dubai - Jumeirah Village Circle Branch'
+    ],
+    'Sharjah & NE Region': [
+        'ENBD - Ajman - Ajman City Center Branch',
+        'ENBD - Ajman - Ajman Main Branch',
+        'ENBD - Fujairah - Fujairah Main Branch',
+        'ENBD - Ras Al Khaimah - Ras AL Khaimah Corniche Branch',
+        'ENBD - Sharjah - Sharjah City Center Branch',
+        'ENBD - Sharjah - Sahara Mall Branch',
+        'ENBD - Sharjah - Matajer Al Quoz Branch',
+        'ENBD - Sharjah - Sharjah Main Branch',
+        'ENBD - Sharjah - Al Zahia City Centre Branch',
+        'ENBD - Sharjah - Sharjah AlSuyouh Branch',
+        'ENBD - Umm Al Quwain - Umm Al Quwain Branch'
+    ]
+}
+
+region_to_branch_ei = {
+    'Abu Dhabi & Al Ain Region': [
+        'EI - Abu Dhabi - Khalifa City Branch',        
+        'EI - Abu Dhabi - Abu Dhabi Mall Branch',
+        'EI - Abu Dhabi - Bawabet Al Sharq Branch',
+        'EI - Al Ain - Jimi Mall Branch',
+        'EI - Abu Dhabi - Abu Dhabi Branch',
+        'EI - Abu Dhabi - Main Branch Abu Dhabi',        
+        'EI - Al Ain - Al Ain Branch'
+    ],
+    'Bur Dubai Region': [
+        'EI - Dubai - Diyafa Road Branch',
+        'EI - Dubai - Bur Dubai Branch',
+        'EI - Dubai - Sheik Zayed Road Branch',
+        'EI - Dubai - Healthcare City Branch',
+        'EI - Dubai - Jumairah Road Branch',
+        'EI - Dubai - Al Wasl Road Branch',
+        'EI - Dubai - Al Barsha Mall Branch',
+        'EI - Dubai - Dubai Mall Branch',
+        'EI - Dubai - Al Barsha Heights Branch',
+        'EI - Dubai - Dubai Hills Branch'
+    ],
+    'Deira Region': [
+        'EI - Dubai - Dubai Festival City Branch',
+        'EI - Dubai - Warqaa Mall Branch',
+        'EI - Dubai - Al Mizher Branch',
+        'EI - Dubai - Nad Al Hamar Branch',
+        'EI - Dubai - Al Tawar Branch',
+        'EI - Dubai - Nad El Shiba',
+        'EI - Dubai - Baniyas Branch',
+        'EI - Dubai - Dubai Land Dept Branch',
+        'EI - Dubai - Khawaneej Walk Branch',
+        'EI - Dubai - Silicon Central Mall Branch'
+    ],
+    'Sharjah & NE Region': [
+        'EI - Sharjah - Halwan Branch',
+        'EI - Sharjah - Al Rahmaniya Mall Branch', 
+        'EI - Sharjah - Juriena Zero 6 Mall Branch',
+        'EI - Sharjah - Qasimiyah Branch',
+        'EI - Sharjah - Khorfakkan Branch',
+        'EI - Ras Al Khaimah - Ras Al Khaimah Branch',
+        'EI - Ajman - Ajman Kalifa Bin Zayed Branch',
+        'EI - Fujairah - Fujairah Branch'
+    ]
+}
+
